70808be14dbb63ad515d29a3151a8a49249f0aa2
Fathurrahman <fathur.ashter15@gmail.com>
2023-11-30 20:21:34 +0800

    final part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,65 +1,68 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useState } from "react"
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: "Fundamentals of React",
-        excercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        excercises: 7
-      },
-      {
-        name: "State of a component",
-        excercises: 14
-      }
-    ]
-  };
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const feedbackTypes = [
+    'good', 'neutral', 'bad'
+  ];
+
+  const handleButton = (feedbackType) => {
+    const newFeedback = { ...feedback };
+    newFeedback[feedbackType] = feedback[feedbackType] + 1;
+    setFeedback(newFeedback);
+  }
+  console.log("Feedback State", feedback);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give feedback</h1>
+      <div>
+        {feedbackTypes.map((type, i) => <Button key={i} text={type} onSmash={() => handleButton(type)} />)}
+      </div>
+      <h2>Statistics</h2>
+      <Statistics feedbackTypes={feedbackTypes} feedbacks={feedback} />
+    </div>
+  )
+};
+
+const Statistics = ({ feedbacks, feedbackTypes }) => {
+  const { good, neutral, bad } = feedbacks;
+
+  const sumFeedback = () => bad + good + neutral;
+  const average = () => {
+    const total = (good * 1) + (bad * -1);
+    const sum = sumFeedback();
+    return sum == 0 ? '0' : (total / sum)
+  }
+  const percentagePositive = () => {
+    const total = good;
+    const sum = sumFeedback();
+    return (sum == 0 || total == 0 ? 0 : ((total / sum) * 100).toFixed(2)) + '%'
+  }
+
+  console.log(feedbackTypes);
+
+  if ((good + neutral + bad) == 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          {feedbackTypes.map((type, i) => <Display key={i} text={type} counter={feedbacks[type]} />)}
+          <Display text="All" counter={sumFeedback()} />
+          <Display text="Average" counter={average()} />
+          <Display text="Positive" counter={percentagePositive()} />
+        </tbody>
+      </table>
     </div>
   )
 }
 
-
-const Part = (props) => {
-  const name = props.part.name;
-  const excercises = props.part.excercises;
-  return (
-    <p>{name} {excercises}</p>
-  )
-}
-const Content = (props) => {
-  const parts = props.parts;
-  const elements = [];
-  parts.forEach(part => {
-    elements.push(<Part part={part} />);
-  })
-  return (
-    <>
-      {elements}
-    </>
-  );
-}
-
-const Total = (props) => {
-  const parts = props.parts;
-  let total = 0;
-  parts.forEach(part => {
-    total += part.excercises;
-  });
-  return <p>Number of exercises {total}</p>
-}
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-export default App
+const Display = ({ text, counter }) => (<tr><td>{text}</td><td>{counter}</td></tr>)
+const Button = ({ onSmash, text }) => (<button onClick={onSmash}>{text}</button>)
+export default App;
