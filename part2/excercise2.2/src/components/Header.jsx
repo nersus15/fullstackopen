e12c19e6e47439496061d76de83cc1856ffea38e
Fathurrahman <fathur.ashter15@gmail.com>
2023-12-05 22:24:59 +0800

    add: exercise 2.2 - 2.3

---
+++ part2/excercise2.2/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+const Header = ({name}) => {
+    return (
+       <h1>{name}</h1>
+    )
+}
+
+Header.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
+export default Header;
